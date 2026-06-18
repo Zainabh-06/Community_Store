51e1bc3b9f50ab9b88badfb1eeec222856b8f5c5
Zainabh-06 <zainabh197006@gmail.com>
2026-06-18 15:41:21 +0530

    Delete frontend_cs directory

--- frontend_cs/src/services/api.js
+++
@@ -1,7 +0,0 @@
-const API_URL = "http://localhost:5000";
-
-export const getProducts = async () => {
-  const response = await fetch(`${API_URL}/products`);
-  const data = await response.json();
-  return data;
-};
